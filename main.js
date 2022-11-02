45bb65bc4791946dda20085940becfdbb6f6263d
PlatypusExplorer <86210928+PlatypusExplorer@users.noreply.github.com>
2022-11-01 19:01:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function backcolor(){
+    document.getElementById("button1").style.backgroundColor = "rgb((Math.floor(Math.random()*200)),(Math.floor(Math.random()*200)),(Math.floor(Math.random()*200)))";
+    console.log (Math.floor(Math.random()*200));
+}
